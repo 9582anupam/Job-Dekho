1fd3552b4bdee1274943b35c3a37bb5b3d39b5e2
Anupam <9582anupamk@gmail.com>
2024-09-03 19:07:32 +0530

    companies marquee

--- frontend/src/components/Pages/Home/components/Companies.js
+++ frontend/src/components/Pages/Home/components/Companies.js
@@ -1,14 +1,25 @@
+import Marquee from "react-fast-marquee";
+import img1 from "../../../../utils/icons/img1.png";
+import img2 from "../../../../utils/icons/img2.png";
+import img3 from "../../../../utils/icons/img3.png";
+import img4 from "../../../../utils/icons/img4.png";
+import img5 from "../../../../utils/icons/img5.png";
+import img6 from "../../../../utils/icons/img6.png";
+import img7 from "../../../../utils/icons/img7.png";
 
-
-const Companies = () => {
-    return (
-        <div className="min-h-28 bg-red-200">
-            <div className="">
-
-            </div>
-            
-        </div>
-    );
-};
+const Companies = () => (
+    <Marquee>
+      <div className="flex space-x-6 pr-6"> 
+        {[img1, img2, img3, img4, img5, img6, img7].map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt={`Logo ${index + 1}`}
+            className="" 
+          />
+        ))}
+      </div>
+    </Marquee>
+  );
 
 export default Companies;
